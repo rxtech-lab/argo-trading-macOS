99021814b1060a4aa7527d522dd1f7d6a8683208
sirily11 <32106111+sirily11@users.noreply.github.com>
2026-02-19 10:05:18 +0800

    feat: add sparkle auto update feature

---
+++ ArgoTradingSwift/Services/UpdaterDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  UpdaterDelegate.swift
+//  ArgoTradingSwift
+//
+//  Created by Claude on 2/19/26.
+//
+
+import Foundation
+import Sparkle
+
+class UpdaterDelegate: NSObject, SPUUpdaterDelegate {
+    deinit {
+        print("UpdaterDelegate deinit")
+    }
+}
